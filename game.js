ff80ee16ee83876dafdac73078081098bb70430d
Alexandr <you@example.com>
2022-10-25 01:28:02 +0300

    add array blocks on Canvas

--- game.js
+++ game.js
@@ -2,49 +2,74 @@ let game = {
     ctx: null,
     platform: null,
     ball: null,
+    blocks: [],
+    rows: 4,
+    cols: 8,
     sprites: {
         background: null,
         ball: null,
         platform: null,
         block: null
     },
-    init: function () {
+    init() {
         this.ctx = document.getElementById("mycanvas").getContext("2d");
     },
     preload (callback) {
         let loaded = 0
         let required = Object.keys(this.sprites).length
+        let onImageLoad = () => {
+            ++loaded
+            if (loaded >= required) {
+                callback() //выполняется коллбек генерации
+            }
+        }
         for (const spritesKey in this.sprites) {
             this.sprites[spritesKey] = new Image()
             this.sprites[spritesKey].src = `img/${spritesKey}.png`
-            this.sprites[spritesKey].addEventListener("load", () => {
-                ++loaded
-                if (loaded >= required) {
-                    callback()
-                }
-            });
+            this.sprites[spritesKey].addEventListener("load", onImageLoad()); //загрузка динамического пути
         }
 
     },
-    run: function (){
+    create() {
+        for (let row = 0; row < this.rows; row++) {
+            for (let col = 0; col < this.cols; col++) {
+                this.blocks.push({
+                    x: (60 + 4) * col + 65, //отступы по 4 пикселя (+ 65 это смещение всей конструкции вправо)
+                    y: (20 + 4) * row + 20 //здесь +20 отступ сверху
+                })
+            }
+
+        }
+    },
+    run(){
         window.requestAnimationFrame(() => {
-            this.render()
+            this.render() //отрисовка запланированной анимации
         });
     },
 
-    render: function (){
+    render() {
+        //начальные координаты всех спрайтов
         this.ctx.drawImage(this.sprites.background, 0, 0);
         this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x,this.ball.y, this.ball.width, this.ball.height);
         this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
-    },
 
+        this.renderBlocks()
+    },
+    renderBlocks() {
+        for(let block of this.blocks) {
+            this.ctx.drawImage(this.sprites.block, block.x, block.y); // рендер блоков
+        }
+    },
     start: function() {
         this.init()
         this.preload(()=> {
+            this.create();
             this.run()
         })
     }
 };
+
+//свойства спрайтов
 
 game.ball = {
     x: 320,
