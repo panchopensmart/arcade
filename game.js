8feb8b55ffe122f0d70f8c07b8f14fc55b76c8d5
Alexandr <you@example.com>
2022-12-13 20:46:29 +0300

    add to follow conflict ball with blocks on console and fix html

--- game.js
+++ game.js
@@ -11,6 +11,8 @@ let game = {
     blocks: [],
     rows: 4,
     cols: 8,
+    height: 360,
+    width: 640,
     sprites: {
         background: null,
         ball: null,
@@ -52,6 +54,8 @@ let game = {
         for (let row = 0; row < this.rows; row++) {
             for (let col = 0; col < this.cols; col++) {
                 this.blocks.push({
+                    width: 60,
+                    height: 20,
                     x: (60 + 4) * col + 65, //отступы по 4 пикселя (+ 65 это смещение всей конструкции вправо)
                     y: (20 + 4) * row + 20 //здесь +20 отступ сверху
                 })
@@ -63,6 +67,12 @@ let game = {
     update() {
         this.platform.move()
         this.ball.move()
+
+        for (let block of this.blocks) {
+            if(this.ball.collide(block)) {
+                console.log('collide!')
+            }
+        }
     },
     run(){
         window.requestAnimationFrame(() => {
@@ -73,6 +83,7 @@ let game = {
     },
 
     render() {
+        this.ctx.clearRect(0,0, 640, 360) //очистка поля
         //начальные координаты всех спрайтов
         this.ctx.drawImage(this.sprites.background, 0, 0)
         this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x,this.ball.y, this.ball.width, this.ball.height)
@@ -84,6 +95,9 @@ let game = {
         for(let block of this.blocks) {
             this.ctx.drawImage(this.sprites.block, block.x, block.y) // рендер блоков
         }
+    },
+    random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
     },
     start() {
         this.init()
@@ -105,12 +119,26 @@ game.ball = {
     height: 20,
     start() {
         this.dy = -this.velocity
+        this.dx = game.random(-this.velocity, this.velocity)
     },
     move() {
         if (this.dy) {
             this.y += this.dy
         }
-    }
+        if (this.dx) {
+            this.x += this.dx
+        }
+    },
+    collide(element) {
+        let x = this.x + this.dx;
+        let y = this.y + this.dy;
+
+        return x + this.width > element.x &&
+            x < element.x + element.width &&
+            y + this.height > element.y &&
+            y < element.y + element.height;
+
+    },
 };
 
 game.platform = {
